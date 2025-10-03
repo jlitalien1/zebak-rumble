df06d56a46bc461e1028a2e3b3737821977be922
Jason Lit <litalienjason1@gmail.com>
2025-10-03 13:18:18 -0700

    environment set up, file name changes, etc...

--- src/test/java/com/example/PluginLauncher.java
+++ src/test/java/com/example/PluginLauncher.java
@@ -3,11 +3,11 @@ package com.example;
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PluginLauncher
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(com.zebakrumble.ZebakRumblePlugin.class);
 		RuneLite.main(args);
 	}
 }
